88b5e4ec069194e832aa4a1d27b95ef7b561bdb9
Carlos Castro Miranda <cjcastromiranda@gmail.com>
2024-02-19 16:21:18 -0500

    updated index.html to have sections

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
